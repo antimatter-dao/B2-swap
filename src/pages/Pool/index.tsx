af7dec188bb53a7944d9e0599c9509590976e2ff
yuhuakukude <15577334054@163.com>
2022-10-22 09:20:20 +0800

    Add more token

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -193,14 +193,14 @@ export default function Pool() {
               </EmptyProposals>
             ) : allV2PairsWithLiquidity?.length > 0 || stakingPairs?.length > 0 ? (
               <>
-                <ButtonSecondary>
-                  <RowBetween>
-                    <ExternalLink href={'https://uniswap.info/account/' + account}>
-                      Account analytics and accrued fees
-                    </ExternalLink>
-                    <span> ↗</span>
-                  </RowBetween>
-                </ButtonSecondary>
+                {/*<ButtonSecondary>*/}
+                {/*  <RowBetween>*/}
+                {/*    <ExternalLink href={'https://uniswap.info/account/' + account}>*/}
+                {/*      Account analytics and accrued fees*/}
+                {/*    </ExternalLink>*/}
+                {/*    <span> ↗</span>*/}
+                {/*  </RowBetween>*/}
+                {/*</ButtonSecondary>*/}
                 {v2PairsWithoutStakedAmount.map(v2Pair => (
                   <FullPositionCard key={v2Pair.liquidityToken.address} pair={v2Pair} />
                 ))}
